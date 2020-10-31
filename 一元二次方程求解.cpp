23e79119378d4161cd2dc8f04df5eebd4d256e60
LongQiangZ <68769178+LongqiangZ@users.noreply.github.com>
2020-10-31 09:04:29 +0800

    一元二次方程

---
+++ 一元二次方程求解.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    double a,b,c,delta,x1,x2;
+	cout<<"Enter a,b,c:";
+	cin>>a>>b>>c;
+	delta = b*b-4*a*c;
+    if(a==0)
+	{
+		if(b==0)
+		{
+			if(c==0)
+				cout<<"ÎÞÇî½â"; 
+			else
+			    cout<<"³ö´í";			
+		}
+		else
+		     cout<<"x="<<-c/b;
+	 } 
+	 else if(delta>0)
+	 {
+	   x1 = (-b+sqrt(delta))/2.0/a;
+	   x2 = (-b-sqrt(delta))/2.0/a;
+	   cout<<"x1="<<x1<<","<<"x2="<<x2<<endl;	
+	 }
+	 else if(delta==0)
+	 {
+	   x1=x2=-b/2.0/a;
+	   cout<<"x1=x2="<<x1<<endl;
+     }
+     else
+     {
+       cout<<"x1="<<-2.0*a/b<<"+"<<sqrt(delta)/2.0/a<<"i"<<",";
+       cout<<"x2="<<-2.0*a/b<<"-"<<sqrt(delta)/2.0/a<<"i"<<endl;
+	 }
+	return 0;
+}
